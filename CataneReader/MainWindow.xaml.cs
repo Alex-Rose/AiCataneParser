5e0fa22dd21b4485d52f08891fac23f4dc663388
Alex-Rose <alexandre.rose.0@gmail.com>
2015-04-15 11:20:41 -0400

    Added files

---
+++ CataneReader/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CataneReader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Reader reader;
+
+        protected List<string> Maps;
+
+
+
+        public int Current
+        {
+            get { return (int)GetValue(CurrentProperty); }
+            set { SetValue(CurrentProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Current.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CurrentProperty =
+            DependencyProperty.Register("Current", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+
+
+        public int Total
+        {
+            get { return (int)GetValue(TotalProperty); }
+            set { SetValue(TotalProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Total.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TotalProperty =
+            DependencyProperty.Register("Total", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            reader = new Reader(@"D:\Source\Python\aiCatane\Source\out.txt");
+
+            reader.Parsed += reader_Parsed;
+
+            DataContext = this;
+
+            reader.Parse();
+        }
+
+        void reader_Parsed(object sender, List<string> e)
+        {
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                Maps = e;
+                TheMap.Text = Maps[0];
+                Current = 0;
+                Total = Maps.Count;
+            }));
+        }
+
+        private void Previous(object sender, RoutedEventArgs e)
+        {
+            if (Current == 0)
+            {
+                Current = Maps.Count;
+            }
+
+            Current--;
+
+            TheMap.Text = Maps[Current];
+        }
+
+        private void Next(object sender, RoutedEventArgs e)
+        {
+            if (Current == Maps.Count - 1)
+            {
+                Current = 0;
+            }
+            else 
+            {
+                Current++;
+            }
+
+            TheMap.Text = Maps[Current];
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if ((int)e.NewValue > 0 && (int)e.NewValue < Maps.Count)
+            {
+                Current = (int)e.NewValue;
+                TheMap.Text = Maps[Current];
+            }
+        }
+    }
+}
---
+++ CataneReader/Reader.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Windows.Threading;
+using System.Windows;
+
+namespace CataneReader
+{
+    class Reader
+    {
+        protected FileSystemWatcher _watcher;
+        protected string _filePath;
+        protected string _fileName;
+        protected List<string> _maps;
+
+        public event EventHandler<List<string>> Parsed;
+
+        public Reader(string path)
+        {
+            _filePath = path;
+            var directory = Path.GetDirectoryName(path);
+
+            _fileName = Path.GetFileName(path);
+
+            _watcher = new FileSystemWatcher(directory);
+
+            _watcher.Changed += _watcher_Changed;
+            _watcher.Created += _watcher_Created;
+
+            _watcher.EnableRaisingEvents = true;
+        }
+
+        void _watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            
+        }
+
+        void _watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            if (e.Name == _fileName)
+            {
+                Console.WriteLine("File changed");
+
+                Thread.Sleep(1000);
+                Parse();
+
+                Parsed.Invoke(this, _maps);
+            }
+        }
+
+        public void Parse()
+        {
+            try
+            {
+                var lines = File.ReadAllLines(_filePath);
+
+                var maps = new List<string>();
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i] == "�tat apr�s le placement initial:" || lines[i] == "�tat � la fin du tour")
+                    {
+                        string map = "";
+                        i++;
+                        while (i < lines.Length && !lines[i].StartsWith("Joueur"))
+                        {
+                            map += lines[i] + "\r\n";
+                            i++;
+                        }
+
+                        maps.Add(map);
+                    }
+                }
+
+                _maps = maps;
+            }
+            catch (Exception)
+            {
+                Thread.Sleep(1000);
+                Parse();
+            }
+        }
+    }
+}
